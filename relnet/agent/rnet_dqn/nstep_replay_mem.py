820f88bf6f3403d2b61d1ef3337531437e250b94
phil-redford <philiprj2@gmail.com>
2021-07-12 22:16:00 +0100

    Updates

--- relnet/agent/rnet_dqn/nstep_replay_mem.py
+++ relnet/agent/rnet_dqn/nstep_replay_mem.py
@@ -1,30 +1,48 @@
 import random
 import numpy as np
 
-class NstepReplaySubMemCell(object):
-    def __init__(self, memory_size):
-        self.memory_size = memory_size
 
+class NstepReplaySubMemCell(object):
+    """
+    Create a memory buffer to sample experience from
+    """
+    def __init__(self, memory_size):
+        """
+        :param memory_size: Size of the replay buffer - newer experience will push out older
+        """
+        # Init parameters
+        self.memory_size = memory_size
         self.actions = [None] * self.memory_size
         self.rewards = [None] * self.memory_size
         self.states = [None] * self.memory_size
         self.s_primes = [None] * self.memory_size
         self.terminals = [None] * self.memory_size
-
+        # Counters for sampling
         self.count = 0
         self.current = 0
 
     def add(self, s_t, a_t, r_t, s_prime, terminal):
+        """
+        Adds a new experience to the buffer
+        :param s_t: State
+        :param a_t: Action
+        :param r_t: Reward
+        :param s_prime: Next State
+        :param terminal: Terminal bool
+        """
         self.actions[self.current] = a_t
         self.rewards[self.current] = r_t
         self.states[self.current] = s_t
         self.s_primes[self.current] = s_prime
         self.terminals[self.current] = terminal
-
+        # Increment counter and set the current position in the buffer
         self.count = max(self.count, self.current + 1)
         self.current = (self.current + 1) % self.memory_size
 
     def add_list(self, list_st, list_at, list_rt, list_sp, list_term):
+        """
+        Adds a list of experience in a loop
+        """
         for i in range(len(list_st)):
             if list_sp is None:
                 sp = (None, None, None)
@@ -33,14 +51,20 @@ class NstepReplaySubMemCell(object):
             self.add(list_st[i], list_at[i], list_rt[i], sp, list_term[i])
 
     def sample(self, batch_size):
+        """
+        Samples a set of experience from the buffer
+        :param batch_size: Sample batch size
+        :return: list_st, list_at, list_rt, list_s_primes, list_term
+        """
+        # Check we have enough experience in memory
         assert self.count >= batch_size
-
+        # Set lists for return
         list_st = []
         list_at = []
         list_rt = []
         list_s_primes = []
         list_term = []
-        
+        # Sample buffer randomly with replacement
         for i in range(batch_size):
             idx = np.random.randint(0, self.count - 1)
             list_st.append(self.states[idx])
@@ -48,10 +72,11 @@ class NstepReplaySubMemCell(object):
             list_rt.append(float(self.rewards[idx]))
             list_s_primes.append(self.s_primes[idx])
             list_term.append(self.terminals[idx])
-
         return list_st, list_at, list_rt, list_s_primes, list_term
 
+
 def hash_state_action(s_t, a_t):
+    # Creates a hash integer for a given state action
     key = s_t[0]
     base = 179424673
     for e in s_t[1].directed_edges:
@@ -64,6 +89,7 @@ def hash_state_action(s_t, a_t):
     
     key = (key * base + a_t) % base
     return key
+
 
 class NstepReplayMemCell(object):
     def __init__(self, memory_size, balance_sample = False):
@@ -94,8 +120,9 @@ class NstepReplayMemCell(object):
         
         return list_st + list_st2, list_at + list_at2, list_rt + list_rt2, list_s_primes + list_s_primes2, list_term + list_term2
 
+
 class NstepReplayMem(object):
-    def __init__(self, memory_size, n_steps, balance_sample = False):
+    def __init__(self, memory_size, n_steps, balance_sample=False):
         self.mem_cells = []
         for i in range(n_steps - 1):
             self.mem_cells.append(NstepReplayMemCell(memory_size, False))
@@ -115,8 +142,7 @@ class NstepReplayMem(object):
                 sp = list_sp[i]
             self.add(list_st[i], list_at[i], list_rt[i], sp, list_term[i], t)
 
-
-    def sample(self, batch_size, t = None):
+    def sample(self, batch_size, t=None):
         if t is None:
             t = np.random.randint(self.n_steps)
 
