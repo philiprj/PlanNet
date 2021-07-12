577543debcca6980f430f8b4898cbbc161f445e2
phil-redford <philiprj2@gmail.com>
2021-07-12 20:46:28 +0100

    Updates

--- relnet/spectral_properties.py
+++ relnet/spectral_properties.py
@@ -4,6 +4,12 @@ import scipy as sp
 
 
 def get_laplacian(A):
+    """
+    Used in field vector agent
+    :param A: Adjacency matrix for network
+    :return: Laplacian matrix
+    """
+    # Sum along row, form diagonal matrix and take A from diagonal matrix.
     degs = A.sum(axis=1).reshape(-1)
     D = np.diagflat(degs)
     L = D - A
@@ -11,12 +17,26 @@ def get_laplacian(A):
 
 
 def compute_fiedler_vector(L, v0=None):
+    """
+    Computes Fieldler Vector
+    :param L: Laplacian matrix
+    :param v0: Starting vector for iteration. Default: random
+    :return: Fieldler Vector
+    """
+    # Computes eigenvectors
     _, eigvs = sp.sparse.linalg.eigsh(L, k=2, which='SM', return_eigenvectors=True, v0=v0)
+    # Fieldler Vector is the 2nd eigenvector
     fiedler_vector = eigvs[:, 1]
     return fiedler_vector
 
 
 def compute_effective_resistance(L):
+    """
+    Computes effective resistance as described in Wang et al. 2014
+    :param L: Laplacian matrix
+    :return: effective resistance
+    """
+    # Gets the eigenvalues - sorts in descending absolute order then takes all but the final values
     eigs = np.sort(np.abs(sp.linalg.eigvalsh(L)))[::-1][:-1]
     # as described in Wang et al. 2014, divide by total number of possible edges
     N = L.shape[0]
@@ -25,4 +45,5 @@ def compute_effective_resistance(L):
 
 
 def get_pseudoinverse(M):
+    # Takes the pseudo-inverse of a matrix
     return np.linalg.pinv(M)
